428d633ae5569638bc35e627db60afea58bbb97b
Denys <yolm9yom@gmail.com>
2025-08-25 02:52:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write code here
+const div = document.querySelector('div');
+const img = document.querySelector('img');
+
+function cenrting(back, obj) {
+  obj.style.left = back.clientWidth / 2 - obj.clientWidth / 2 + 'px';
+  obj.style.top = back.clientHeight / 2 - obj.clientHeight / 2 + 'px';
+}
+
+cenrting(div, img);
