4392527259277b91354f14d39e2ae4daf46e7483
Denys <yolm9yom@gmail.com>
2025-08-25 03:06:36 +0300

    make build

---
+++ index.f2a92f8d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");function i(){e.style.position="relative",t.style.position="absolute";var i=(e.clientWidth-t.clientWidth)/2,l=(e.clientHeight-t.clientHeight)/2;t.style.left=i+"px",t.style.top=l+"px"}t.complete?i():t.addEventListener("load",i),window.addEventListener("resize",i);
+//# sourceMappingURL=index.f2a92f8d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+function centering() {
+    wall.style.position = "relative";
+    spider.style.position = "absolute";
+    const lefting = (wall.clientWidth - spider.clientWidth) / 2;
+    const toping = (wall.clientHeight - spider.clientHeight) / 2;
+    spider.style.left = lefting + "px";
+    spider.style.top = toping + "px";
+}
+if (spider.complete) {
+    centering();
+    window.addEventListener("resize", centering);
+} else {
+    spider.addEventListener("load", centering);
+    window.addEventListener("resize", centering);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
