37b6889720ef1c97aba3f5025220bc8c07fcfd7f
Denys <yolm9yom@gmail.com>
2025-08-25 03:01:55 +0300

    add2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const div = document.querySelector('div');
-const img = document.querySelector('img');
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
 
-function cenrting(back, obj) {
-  obj.style.left = back.clientWidth / 2 - obj.clientWidth / 2 + 'px';
-  obj.style.top = back.clientHeight / 2 - obj.clientHeight / 2 + 'px';
+function centering() {
+  wall.style.position = 'relative';
+  spider.style.position = 'absolute';
+
+  const lefting = (wall.clientWidth - spider.clientWidth) / 2;
+  const toping = (wall.clientHeight - spider.clientHeight) / 2;
+
+  spider.style.left = lefting + 'px';
+  spider.style.top = toping + 'px';
 }
 
-cenrting(div, img);
+if (spider.complete) {
+  centering();
+} else {
+  spider.addEventListener('load', centering);
+}
